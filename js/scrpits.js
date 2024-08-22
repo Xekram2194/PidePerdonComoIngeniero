fc32ecfc90b415389e64832117cdd3daa50ba980
MARCO ANTONIO MENDOZA HUILLCA <mmendozah@uandina.edu.pe>
2024-08-22 09:33:19 -0500

    last sorry page

--- js/scrpits.js
+++ js/scrpits.js
@@ -48,23 +48,35 @@ document.getElementById('disculpaForm').addEventListener('submit', async functio
     // Solo si todos los campos son válidos, se procede a generar y acortar el enlace
     if (valid) {
         // Crear un enlace único para la disculpa
-        const link = `perdon.html?nombre=${encodeURIComponent(nombre)}&mensaje=${encodeURIComponent(mensaje)}&mensajeLargo=${encodeURIComponent(mensajeLargo)}`;
-        
+        const link = `perdon.html?nombre=${encodeURIComponent(nombre.value)}&mensaje=${encodeURIComponent(mensaje.value)}&mensajeLargo=${encodeURIComponent(mensajeLargo.value)}`;
         const shortLink = await acortarEnlace(`${window.location.origin}/${link}`);
 
-        const linkContainer = document.createElement('div');
-        linkContainer.innerHTML = `
-            <p>Tu link ha sido generado:</p>
-            <a href="${shortLink}" target="_blank">${shortLink}</a>
-            <button id="copiarLink">Copiar Link</button>
-        `;
-        document.querySelector('.container').innerHTML = '';
-        document.querySelector('.container').appendChild(linkContainer);
+        // Mostrar el modal con el enlace generado
+        const modal = document.getElementById('modal');
+        const modalLink = document.getElementById('modalLink');
+        modalLink.href = shortLink;
+        modalLink.textContent = shortLink;
 
-        document.getElementById('copiarLink').addEventListener('click', function() {
+        // Mostrar el modal
+        modal.style.display = 'block';
+
+        // Copiar el enlace al portapapeles
+        document.getElementById('copiarLinkModal').addEventListener('click', function() {
             navigator.clipboard.writeText(shortLink).then(() => {
                 alert('Enlace copiado al portapapeles');
             });
+        });
+
+        // Cerrar el modal
+        document.getElementById('closeModal').addEventListener('click', function() {
+            modal.style.display = 'none';
+        });
+
+        // Cerrar el modal si se hace clic fuera del contenido del modal
+        window.addEventListener('click', function(event) {
+            if (event.target == modal) {
+                modal.style.display = 'none';
+            }
         });
     }
 });
